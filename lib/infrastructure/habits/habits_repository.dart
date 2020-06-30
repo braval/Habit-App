6860a7588ac7c89f938e6a4b1d46af0deddc28ca
Brijen Raval <braval@google.com>
2020-06-30 09:04:46 -0700

    Update count when done

--- lib/infrastructure/habits/habits_repository.dart
+++ lib/infrastructure/habits/habits_repository.dart
@@ -8,7 +8,6 @@ import 'package:dartz/dartz.dart';
 import 'package:habits/domain/habits/i_habits_repository.dart';
 import 'package:habits/infrastructure/habits/habit_dtos.dart';
 import 'package:injectable/injectable.dart';
-import 'package:intl/intl.dart';
 import 'package:habits/infrastructure/core/firestore_helpers.dart';
 
 @LazySingleton(as: IHabitsRepository)
@@ -22,20 +21,17 @@ class HabitsRepository implements IHabitsRepository {
   Future<Either<HabitFailure, Unit>> add(HabitItem habitItem) async {
     try {
       final HabitItemDto habitItemDto = HabitItemDto.fromDomain(habitItem);
-      final today = DateFormat("yyyy-MM-dd").format(DateTime.now());
       final userDoc = await _firestore.userDocument();
       final dailyHabitCollection = userDoc.dailyHabitsCollection;
 
-      final existingHabit =
-          await dailyHabitCollection.document(habitItemDto.name).get();
-      if (existingHabit.exists) {
+      final newHabitDocumentRef =
+          dailyHabitCollection.document(habitItemDto.name);
+
+      final habit = await newHabitDocumentRef.get();
+      if (habit.exists) {
         return left(const HabitFailure.habitAlreadyExists());
       }
-
-      await dailyHabitCollection
-          .document(habitItemDto.name)
-          .setData(habitItemDto.toJson());
-
+      await newHabitDocumentRef.setData(habitItemDto.toJson());
       return right(unit);
     } on PlatformException catch (_) {
       return left(const HabitFailure.unexpected());
@@ -45,14 +41,11 @@ class HabitsRepository implements IHabitsRepository {
   @override
   Future<Either<HabitFailure, Unit>> delete(HabitItem habitItem) async {
     try {
-      final today = DateFormat("yyyy-MM-dd").format(DateTime.now());
       final userDoc = await _firestore.userDocument();
       final dailyHabitCollection = userDoc.dailyHabitsCollection;
 
-      final habitRef = dailyHabitCollection
-          .document(today)
-          .habitsCollection
-          .document(habitItem.id.getOrCrash());
+      final habitRef =
+          dailyHabitCollection.document(habitItem.name.getOrCrash());
 
       await habitRef.delete();
       return right(unit);
@@ -64,16 +57,13 @@ class HabitsRepository implements IHabitsRepository {
   @override
   Future<Either<HabitFailure, Unit>> update(HabitItem habitItem) async {
     try {
-      final today = DateFormat("yyyy-MM-dd").format(DateTime.now());
       final userDoc = await _firestore.userDocument();
       final dailyHabitCollection = userDoc.dailyHabitsCollection;
+      final habitDocumentRef =
+          dailyHabitCollection.document(habitItem.name.getOrCrash());
 
-      final habitRef = dailyHabitCollection
-          .document(today)
-          .habitsCollection
-          .document(habitItem.id.getOrCrash());
-
-      await habitRef.updateData(HabitItemDto.fromDomain(habitItem).toJson());
+      await habitDocumentRef
+          .updateData(HabitItemDto.fromDomain(habitItem).toJson());
       return right(unit);
     } on PlatformException catch (_) {
       return left(const HabitFailure.unexpected());
@@ -83,14 +73,10 @@ class HabitsRepository implements IHabitsRepository {
   @override
   Stream<Either<HabitFailure, List<HabitItem>>> watchAll(
       DateTime dateTime) async* {
-    final today = DateFormat("yyyy-MM-dd").format(DateTime.now());
     final userDoc = await _firestore.userDocument();
     final dailyHabitCollection = userDoc.dailyHabitsCollection;
 
-    final habitsCollection =
-        dailyHabitCollection.document(today).habitsCollection.orderBy('done');
-
-    yield* habitsCollection.snapshots().map(
+    yield* dailyHabitCollection.snapshots().map(
       (snapshot) {
         if (snapshot.documents.isNotEmpty) {
           return right(
